8579c5d56edc194135d11d96ec5d477b8ea375f2
DrewnioQ <konro99@gmail.com>
2022-01-12 22:55:13 +0100

    fixed class so it now works just like other script

--- class_detect_fruits.py
+++ class_detect_fruits.py
@@ -10,7 +10,6 @@ from typing import Dict
 
 
 class Fruit:
-
     img = None
 
     def __init__(self, img_hsv, name, hsv_lower, hsv_upper):
@@ -18,44 +17,64 @@ class Fruit:
         self.name = name
         self.hsv_lower = hsv_lower
         self.hsv_upper = hsv_upper
+        self.mask = None
 
-    def img_conv2hsv(self):
-        """Image resize and conv to hsv"""
-
-        img_res = cv2.resize(self.img_hsv, dsize=None, fx=0.25, fy=0.25, interpolation=cv2.INTER_CUBIC)
-        img_hsv = cv2.cvtColor(img_res, cv2.COLOR_BGR2HSV)
-
-        return img_hsv
+    # def img_conv2hsv(self):
+    #     """Image resize and conv to hsv"""
+    #
+    #     img_res = cv2.resize(self.img_hsv, dsize=None, fx=0.25, fy=0.25, interpolation=cv2.INTER_CUBIC)
+    #     img_hsv = cv2.cvtColor(img_res, cv2.COLOR_BGR2HSV)
+    #
+    #     return img_hsv
 
     def create_mask(self):
         """Create a mask with given hsv threshold values"""
 
-        img_hsv = self.img_conv2hsv()
+        img_hsv = self.img_hsv
 
-        mask = cv2.inRange(img_hsv, self.hsv_lower, self.hsv_upper)
+        self.mask = cv2.inRange(img_hsv, self.hsv_lower, self.hsv_upper)
 
-        return mask
+        return self.mask
 
     def find_conts(self):
-        """Finds contours on a given mask"""
+        """Finds contours on a given mask and returns the biggest one"""
 
         mask = self.create_mask()
-
         conts, _ = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         biggest = sorted(conts, key=cv2.contourArea, reverse=True)[0]
 
         return biggest
 
     def get_rect(self):
+        """Returns dimensions of a found rectangle"""
+
         biggest = self.find_conts()
 
         rect = cv2.boundingRect(biggest)
         x, y, w, h = rect
 
         if w < 100 or h < 100:
-            return None
+            x = y = w = h = 0
+            rect = x, y, w, h
+            return rect
 
         return rect
+
+    def draw_rect(self):
+        """Draws a rectangle around found object on original image"""
+
+        x, y, w, h = self.get_rect()
+
+        # try:
+        #     x, y, w, h = self.get_rect()
+        # except TypeError:
+        #     x = y = w = h = 0
+
+        cv2.rectangle(Fruit.img, (x, y), (x + w, y + h), (0, 255, 0), thickness=2)
+        cv2.putText(Fruit.img, self.name, (x, y), cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(0, 255, 255),
+                    thickness=2)
+        # mask_res = cv2.resize(self.mask, dsize=None, fx=0.5, fy=0.5, interpolation=cv2.INTER_CUBIC)
+        cv2.imshow(f"Mask of {self.name.upper()}", self.mask)
 
 
 def detect_fruits(img_path: str) -> Dict[str, int]:
@@ -73,35 +92,27 @@ def detect_fruits(img_path: str) -> Dict[str, int]:
     """
     # TODO: Implement detection method.
     img = cv2.imread(img_path, cv2.IMREAD_COLOR)
-    # img_res = cv2.resize(img, dsize=None, fx=0.25, fy=0.25, interpolation=cv2.INTER_CUBIC)
-    img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    img_res = cv2.resize(img, dsize=None, fx=0.25, fy=0.25, interpolation=cv2.INTER_CUBIC)
+    img_hsv = cv2.cvtColor(img_res, cv2.COLOR_BGR2HSV)
 
-    Fruit.img = img
-    apple = Fruit(img_hsv=img_hsv,
-                  name="apple",
-                  hsv_lower=np.array([0, 50, 50]),
-                  hsv_upper=np.array([9, 255, 255]))
+    Fruit.img = img_res
+    apple  = Fruit(img_hsv=img_hsv,
+                   name="apple",
+                   hsv_lower=np.array([0, 50, 50]),
+                   hsv_upper=np.array([9, 255, 255]))
     banana = Fruit(img_hsv=img_hsv,
                    name="banana",
                    hsv_lower=np.array([24, 75, 75]),
                    hsv_upper=np.array([35, 255, 255]))
     orange = Fruit(img_hsv=img_hsv,
                    name="orange",
-                   hsv_lower=np.array([12, 50, 200]),
+                   hsv_lower=np.array([12, 150, 200]),
                    hsv_upper=np.array([20, 255, 255]))
 
-    apple_rect = apple.get_rect()
-    banana_rect = banana.get_rect()
-    orange_rect = orange.get_rect()
-    fruits_rects = [apple_rect, banana_rect, orange_rect]
+    apple.draw_rect()
+    banana.draw_rect()
+    orange.draw_rect()
 
-    # for fruit_rect in fruits_rects:
-    #     if fruit_rect:
-    #         cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), thickness=2)
-    #         cv2.putText(img, name, (x, y), cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(0, 255, 255), thickness=2)
-    #         mask_res = cv2.resize(mask, dsize=None, fx=0.5, fy=0.5, interpolation=cv2.INTER_CUBIC)
-    #         cv2.imshow(f"Mask {fruit_rect.create_mask()}", mask_res)
-    img_res = cv2.resize(img, dsize=None, fx=0.25, fy=0.25, interpolation=cv2.INTER_CUBIC)
     cv2.imshow("Post", img_res)
 
     cv2.waitKey(0)
